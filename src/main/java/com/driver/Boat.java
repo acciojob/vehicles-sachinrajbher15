6c62d6d62b8068815a2d292b039d0f78097d38bb
SACHIN KUMAR RAJBHER <86919694+sachinrajbher15@users.noreply.github.com>
2024-05-28 21:50:14 +0530

    Update Boat.java

--- src/main/java/com/driver/Boat.java
+++ src/main/java/com/driver/Boat.java
@@ -1,14 +1,15 @@
 package com.driver;
 
-public class Boat implements WaterVehicle {
+public class Boat implements WaterVehicle{
+
     private String name;
     private int capacity;
-    
-    public Boat(String name,int capacity){
+
+    public Boat(String name, int capacity) {
         this.name = name;
         this.capacity = capacity;
     }
-    
+
     public String getVehicleName() {
         return name;
     }
